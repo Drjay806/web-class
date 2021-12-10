e0f8a503a626338656789210f1b92478c08919f4
Drjay806 <70110182+Drjay806@users.noreply.github.com>
2021-12-09 21:22:22 -0600

    change dec 09

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-//does the browser support service workers?
-if ("serviceWorker" in navigator) {
-  //then register our service worker
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((reg) => {
-      //display a success message
-      console.log("Service Worker Registration (Scope: ${reg.scope})");
-    })
-    .catch((error) => {
-      // display an error message
-      console.log("Service Worker Error (${error})");
-    });
-} else {
-}
 if ("serviceWorker" in navigator) {
   // defer service worker installation until page completes loading
   window.addEventListener("load", () => {
--- js/account.js
+++ js/account.js
@@ -1,40 +1,27 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   //
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const showNavbar = (toggleId2, navId2, bodyId2, headerId2) => {
     //set toggle to the id toggle id from above param
-    const toggle = document.getElementById(toggleId),
+    const toggle = document.getElementById(toggleId2),
       //set navId from parma to variable nav
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
+      nav = document.getElementById(navId2),
+      bodypd = document.getElementById(bodyId2),
+      headerpd = document.getElementById(headerId2);
 
     // Validate that all variables exist
     if (toggle && nav && bodypd && headerpd) {
       toggle.addEventListener("click", () => {
         // show navbar
-        nav.classList.toggle("show");
+        nav.classList.toggle("show2");
         // change icon
         toggle.classList.toggle("bx-x");
         // add padding to body
 
         // add padding to header
-        headerpd.classList.toggle("body-pd");
+        headerpd.classList.toggle("body-pd2");
       });
     }
   };
 
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
-
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
-    }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
-
-  // Your code to run since DOM is loaded and ready
+  showNavbar("header-toggle2", "nav-bar2", "body-pd2", "header2");
 });
